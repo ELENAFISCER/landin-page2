b673ca0f67afe4aeed276c6bddeeeb4b654a06c5
elenafiscer <tandem.fiscer@gmail.com>
2024-06-03 13:56:02 +0200

    subo archivos 3/06

---
+++ src/components/button/buttoninfo.js
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+function AlertButton2(){
+  return(
+    <a href="info" role='button' className='button'>Información institucional</a>
+  )
+}
+export default AlertButton2
