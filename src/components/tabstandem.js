5c9f380fb4ea45e565d0f44105be1a4c3b4dcfed
elenafiscer <tandem.fiscer@gmail.com>
2024-06-13 12:00:49 +0200

    tabsmapa

---
+++ src/components/tabstandem.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import { useState } from "react";
+import MapaConMarcador from './mapa';
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+export default function CenteredTabs({ inputValue, setInputValue, latLng, setLatLng, setInputType }) {
+  const [value, setValue] = React.useState(0);
+  const handleLatChange = (event) => {
+    const newLat = parseFloat(event.target.value);
+    if (!isNaN(newLat)) {
+      setLatLng((prev) => ({ ...prev, lat: newLat }));
+    }
+  };
+  const handleLngChange = (event) => {
+    const newLng = parseFloat(event.target.value);
+    if (!isNaN(newLng)) {
+      setLatLng((prev) => ({ ...prev, lng: newLng }));
+    }
+  };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    const inputTypes = ['url', 'text', 'coordinates'];
+    setInputType(inputTypes[newValue]);
+  };
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  return (
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="URL" />
+        <Tab label="Texto" />
+        <Tab label="Coordenadas" />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <h3>Introduce tu URL:</h3>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <h3>Introduce tu Texto:</h3>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <h3>Introduce tus Coordenadas: (Latitud, Longitud)</h3>
+        <label>
+          Latitud:
+          <input type="number" value={latLng.lat} onChange={handleLatChange} />
+        </label>
+        <label>
+          Longitud:
+          <input type="number" value={latLng.lng} onChange={handleLngChange} />
+        </label>
+        <MapaConMarcador setLatLng={setLatLng} />
+        {/* <p> Latitud: {latLng.lat}, Longitud: {latLng.lng}</p> */}
+      </TabPanel>
+    </Box>
+  );
+}
--- src/pages/crear-qr.js
+++ src/pages/crear-qr.js
@@ -6,9 +6,12 @@ import MyComponent from "../components/crearqr-componentes/componente1";
 import QRCode from 'qrcode.react';
 import { toPng, toJpeg, toSvg } from 'html-to-image';
 import download from 'downloadjs';
-
+import TabsTandem from "../components/tabstandem";
+import CenteredTabs from "../components/tabstandem";
 function Crearqr() {
   const [inputValue, setInputValue] = useState('');
+  const [latLng, setLatLng] = useState({ lat: 40.030501, lng: -3.604052 });
+  const [inputType, setInputType] = useState('url');
   const [qrColor, setQrColor] = useState('black');
   const [qrSize, setQrSize] = useState(100);
   const qrRef = useRef(null);
@@ -27,23 +30,18 @@ function Crearqr() {
       download(dataUrl, 'qr-code.png');
     }
   };
-
   const handleDownload2 = async () => {
     if (qrRef.current) {
       const dataUrl = await toJpeg(qrRef.current);
       download(dataUrl, 'qr-code.jpeg');
     }
   };
-
   const handleDownload3 = async () => {
     if (qrRef.current) {
       const dataUrl = await toSvg(qrRef.current);
       download(dataUrl, 'qr-code.svg');
     }
   };
-
-
-
   const containerStyle = {
     backgroundColor: "beige",
     border: "2px solid black",
@@ -54,35 +52,60 @@ function Crearqr() {
   const colorOptions = ['black', 'blue', 'red', 'green', 'yellow', 'purple', 'orange', 'pink', 'magenta']; // Colores ampliados
   return (
     <Layout>
-      <div style={containerStyle}>
+<div style={containerStyle}>
         <h1>GENERADOR DE QR</h1>
-        <Collapse2 />
+        <CenteredTabs
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            latLng={latLng}
+            setLatLng={setLatLng}
+            setInputType={setInputType}
+          />
+        <Collapse2></Collapse2>
         <br />
-        <p>Introduce tu texto o url aquí:</p>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
+       <div className="black">
         <MyComponent
           onColorChange={handleColorChange}
           onSizeChange={handleSizeChange}
           colorOptions={colorOptions}
         />
-        <br /> 
-        <div className="qr-contenido">
-        <div ref={qrRef}>
-          <QRCode value={inputValue} size={qrSize} fgColor={qrColor} />
         </div>
         <br />
-      <br />
+        <div className="qr-contenido">
+        <div ref={qrRef} className="black">
+          <p>Este es tu QR generado</p>
+        <QRCode
+              value={inputType === 'coordinates' ? `${latLng.lat},${latLng.lng}` : inputValue}
+              size={qrSize}
+              fgColor={qrColor}
+            />
+        </div>
+        <br />
+       <br />
          {/* <TandemButton enlace="#">Generar QR</TandemButton> */}
-        <p>Contenido del QR:</p>
         <p>{inputValue}</p>
         </div>
         <br />
+        <div className="black">
         <p>Descargar QR:</p>
         <button onClick={handleDownload} className="button22">Png</button>
         <button onClick={handleDownload2} className="button22">Jpeg</button>
         <button onClick={handleDownload3} className="button22">Svg</button>
+        </div>
       </div>
     </Layout>
   );
 }
 export default Crearqr;
+
+
+
+
+
+
+
+
+
+
+
+
